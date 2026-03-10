1739abfc4bfec5ba7a9a8f1cb9de9712f6037a70
Hamish Briggs <119870385+Something231@users.noreply.github.com>
2026-03-10 19:22:41 +0800

    Refactor Body class and update celestial bodies

--- main.py
+++ main.py
@@ -7,27 +7,41 @@ import math
 import pygame
 
 class Body:
-    def __init__(self, initial_position, initial_velocity, mass, radius) -> None:
+    def __init__(self, initial_position, initial_velocity, mass, radius, color=(255, 255, 255)) -> None:
         self.position = initial_position
         self.velocity = initial_velocity
         self.mass = mass
         self.radius = radius
+        self.color = color
+        self.temp_force = np.array([0, 0])
+    
+    def resolve_force(self, bodies):
+        for b in bodies:
+            if np.linalg.norm(b.position-self.position) != 0:
+                grav = (G * (b.mass * self.mass)) / ((np.linalg.norm(b.position-self.position))**2)
+                self.temp_force = self.temp_force + grav * ((b.position-self.position) / np.linalg.norm(b.position-self.position))
 
-    def resolve_velocity(self, m, p, mult):
-        grav = (G * (m * self.mass)) / ((np.linalg.norm(p-self.position))**2)
-        force = grav * ((p-self.position) / np.linalg.norm(p-self.position))
-        self.velocity = self.velocity + (force / self.mass) * mult
+    def resolve_velocity(self, mult):
+        self.velocity = self.velocity + (self.temp_force / self.mass) * mult
         self.position = self.position + self.velocity * mult
-
-      
+        self.temp_force = np.array([0, 0])
+        
 pygame.init()
 screen = pygame.display.set_mode((1000, 1000))
 
-sun = Body(np.array([0, 0]), np.array([0, 0]), (1.989 * 10**30), 50)
-earth_vel = math.sqrt((G * sun.mass) / Au)
-earth = Body(np.array([0, Au]), np.array([earth_vel, 0]), (5.9722 * 10**24), 10)
 
- 
+sunmass = (1.989 * 10**30)
+sun_vel = math.sqrt((G * sunmass) / Au) / 2
+mars_vel = math.sqrt((G * sunmass) / (1.5 * Au))
+earth_vel = math.sqrt((G * sunmass) / Au)
+
+
+sun = Body(np.array([0, 0]), np.array([0, 0]), sunmass, 50, (255, 165, 0))
+earth = Body(np.array([Au, 0]), np.array([0, earth_vel]), (5.9722 * 10**24), 15, (0, 255, 100))
+mars = Body(np.array([0, -1.5 * Au]), np.array([-mars_vel, 0]), (6.4191 * 10**23), 10, (200, 0, 75))
+evil_sun = Body(np.array([0, Au]), np.array([-sun_vel, 0]), sun.mass * 45/50, 45, (150, 10, 200))
+
+all_bodies = [sun, earth, mars]
 
 #let 1 AU = 100 pix
 def pixelise(position):
@@ -44,13 +58,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.draw.circle(screen, (255, 165, 0), pixelise(sun.position), sun.radius)
-    pygame.draw.circle(screen, (0, 255, 100), pixelise(earth.position), earth.radius)
-    pygame.display.flip()
-
     clock.tick(60)
+    for b in all_bodies:
+        b.resolve_force(all_bodies)
+    for b in all_bodies:
+        b.resolve_velocity(17520)
+        pygame.draw.circle(screen, b.color, pixelise(b.position), b.radius)
+    pygame.display.flip()
+    print(pixelise(sun.position))
 
-    earth.resolve_velocity(sun.mass, sun.position, 35040)
-    print(pixelise(earth.position))
-
+# Quit Pygame
 pygame.quit()
